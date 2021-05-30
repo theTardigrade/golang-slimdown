897684c8c415ad8ada8c378a271d3409f81f575e
James Smith <theTardigrade@users.noreply.github.com>
2021-05-31 00:09:19 +0100

    Use init function in debug package

--- internal/debug/debug.go
+++ internal/debug/debug.go
@@ -6,7 +6,6 @@ import (
 	"math"
 	"strconv"
 	"strings"
-	"sync"
 
 	"github.com/theTardigrade/golang-slimdown/internal/tokenization"
 )
@@ -16,37 +15,32 @@ const (
 )
 
 var (
-	printTokensIntMaxLen           int
-	printTokensIndent              int
-	printTokensIndentCalculateOnce sync.Once
+	printTokensIntMaxLen int
+	printTokensIndent    int
 )
 
-func printTokensIndentCalculate() {
-	printTokensIndentCalculateOnce.Do(func() {
-		var i int
+func init() {
+	var i int
 
-		for increment := 1; i >= 0; increment = int(math.Ceil(float64(increment) * 1.25)) {
-			i += increment
+	for increment := 1; i >= 0; increment = int(math.Ceil(float64(increment) * 1.25)) {
+		i += increment
 
-			if tt := tokenization.TokenType(i); tt.String() == "UNK" {
-				for j, l := i-1, i-increment; j >= l; j-- {
-					if tt = tokenization.TokenType(j); tt.String() != "UNK" {
-						i = -j
-						break
-					}
+		if tt := tokenization.TokenType(i); tt.String() == "UNK" {
+			for j, l := i-1, i-increment; j >= l; j-- {
+				if tt = tokenization.TokenType(j); tt.String() != "UNK" {
+					i = -j
+					break
 				}
 			}
 		}
+	}
 
-		i *= -1
-		printTokensIntMaxLen = len(strconv.Itoa(i))
-		printTokensIndent = printTokensMinIndent + printTokensIntMaxLen
-	})
+	i *= -1
+	printTokensIntMaxLen = len(strconv.Itoa(i))
+	printTokensIndent = printTokensMinIndent + printTokensIntMaxLen
 }
 
 func PrintTokens(tokens *tokenization.TokenCollection) {
-	printTokensIndentCalculate()
-
 	var builder strings.Builder
 
 	for i, t := range tokens.Data {
